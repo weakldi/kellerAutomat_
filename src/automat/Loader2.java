0cfddee048504d2bb00d3c0c1ed5c7f16f75b558
sg09MindererK <sg09MindererK@ID00133214.schulen.stadt-muenster.de>
2016-11-29 13:49:33 +0100

    Test

---
+++ src/automat/Loader2.java
@@ -0,0 +1,14 @@
+package automat;
+
+import java.util.ArrayList;
+
+public class Loader2 {
+	public void load(String data){
+		ArrayList<String> tokens = new ArrayList<>();
+		for(String line:data.split("\n")){
+			if(line.matches("Z\\{[A-Za-z,]*[A-Za-z]\\}")){
+				
+			}
+		}
+	}
+}
